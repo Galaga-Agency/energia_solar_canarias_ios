d770034bde86cb5535158143484f6cf479ff7946
nestorsan77 <nestorsan77@gmail.com>
2025-02-11 12:36:06 +0000

    Energía solar canarias aplicación ios

---
+++ esc-iosTests/esc_iosTests.swift
@@ -0,0 +1,16 @@
+//
+//  esc_iosTests.swift
+//  esc-iosTests
+//
+//  Created by Nestor Calderon perez on 11/2/25.
+//
+
+import Testing
+
+struct esc_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ esc-iosUITests/esc_iosUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  esc_iosUITestsLaunchTests.swift
+//  esc-iosUITests
+//
+//  Created by Nestor Calderon perez on 11/2/25.
+//
+
+import XCTest
+
+final class esc_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
